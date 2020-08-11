60b560c3c1b45e0f465190b6e6cf28f3632f9c71
William Frank <9719801+beridel@users.noreply.github.com>
2020-08-11 17:13:47 -0400

    updated recent pubs to work

--- update_william_pubs.bash
+++ update_william_pubs.bash
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# sync authors
+rsync -a ../tecto_source/content/authors/ ./content/authors/
+# sync pubs
 rsync -a --exclude "_index.md" ../tecto_source/content/publication/ ./content/publication/
