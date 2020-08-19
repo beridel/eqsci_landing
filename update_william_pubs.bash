2ae65b9f4b32d377aa4b9cb486c0baba6a362de9
William Frank <9719801+beridel@users.noreply.github.com>
2020-08-19 15:04:06 -0400

    updated tags

--- update_william_pubs.bash
+++ update_william_pubs.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
+SRC_DIR="/Users/wbfrank/Projects/code/website/tecto_source/content"
 
 # sync authors
-rsync -a ../tecto_source/content/authors/ ./content/authors/
+rsync -a --exclude "_index.md" $SRC_DIR/authors/ ./content/authors/
 # sync pubs
-rsync -a --exclude "_index.md" ../tecto_source/content/publication/ ./content/publication/
+rsync -a --exclude "_index.md" $SRC_DIR/publication/ ./content/publication/
